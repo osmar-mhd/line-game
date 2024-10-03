820cf577cf304970a6906a388a6007f43c069396
osmar-vl <osmarmojarra@gmail.com>
2024-10-02 23:22:33 -0600

    Base

---
+++ spring-line-game/src/main/java/com/org/spring_line_game/SpringLineGameApplication.java
@@ -0,0 +1,13 @@
+package com.org.spring_line_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLineGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLineGameApplication.class, args);
+	}
+
+}
---
+++ spring-line-game/src/test/java/com/org/spring_line_game/SpringLineGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.spring_line_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLineGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
